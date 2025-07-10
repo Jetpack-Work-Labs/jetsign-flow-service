627a1db638469ae3e43106970e63abd97913d86f
pradip <pradip.kharal777@gmail.com>
2025-07-10 13:53:56 +0545

    index

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,21 @@
-import { createCertificate } from "./services/certificate";
-import { createCryptoToken } from "./services/signserver";
-import { signPDF } from "./services/signserver/sign";
-import {
-  enqueueSignJob,
-  HandleQueue,
-  pollAndProcessJobs,
-} from "./services/sqs";
-import { SignJob } from "./interfaces";
-import { createP12Docker, uploadFile } from "./utils";
+import { HandleQueue, pollAndProcessJobs } from "./services/sqs";
 import { config } from "./config";
 import { connectDB } from "./infrastructure";
-import { EnvelopeModel } from "./model";
-import mongoose, { ConnectOptions } from "mongoose";
+
+// (async () => {
+//   try {
+
+//     await enqueueSignJob({
+//       account_id: "6282335",
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// })();
 
 (async () => {
   try {
     await connectDB(config.db);
-
-    await enqueueSignJob({
-      account_id: "6282335",
-    });
-  } catch (error) {
-    console.log(error);
-  }
-})();
-
-(async () => {
-  try {
     await pollAndProcessJobs(HandleQueue);
   } catch (error) {
     console.log(error);
